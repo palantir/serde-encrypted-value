fe2925b70eb794fe7bc93c202e78893f8e64234d
Steven Fackler <sfackler@palantir.com>
2017-02-21 16:06:48 -0800

    Initial commit

---
+++ src/deserializer.rs
@@ -0,0 +1,371 @@
+use serde::de;
+use std::fmt;
+
+use Key;
+
+/// A deserializer which automatically decrypts strings.
+///
+/// Encrypted strings should be formatted like `${enc:<base64 ciphertext here>}`.
+pub struct Deserializer<'a, D> {
+    deserializer: D,
+    key: Option<&'a Key>,
+}
+
+impl<'a, D> Deserializer<'a, D>
+    where D: de::Deserializer
+{
+    /// Creates a new `Deserializer` wrapping another deserializer and decrypting string values.
+    ///
+    /// If `key` is `None`, deserialization will fail if an encrypted string is encountered.
+    pub fn new(deserializer: D, key: Option<&'a Key>) -> Deserializer<'a, D> {
+        Deserializer {
+            deserializer: deserializer,
+            key: key,
+        }
+    }
+}
+
+macro_rules! forward_deserialize {
+    ($name:ident) => {forward_deserialize!($name, );};
+    ($name:ident, $($arg:tt => $ty:ty),*) => {
+        fn $name<V>(self, $($arg: $ty,)* visitor: V) -> Result<V::Value, D::Error>
+            where V: de::Visitor
+        {
+            let visitor = Visitor {
+                visitor: visitor,
+                key: self.key,
+            };
+            self.deserializer.$name($($arg,)* visitor)
+        }
+    }
+}
+
+impl<'a, D> de::Deserializer for Deserializer<'a, D>
+    where D: de::Deserializer
+{
+    type Error = D::Error;
+
+    forward_deserialize!(deserialize);
+    forward_deserialize!(deserialize_bool);
+    forward_deserialize!(deserialize_u8);
+    forward_deserialize!(deserialize_u16);
+    forward_deserialize!(deserialize_u32);
+    forward_deserialize!(deserialize_u64);
+    forward_deserialize!(deserialize_i8);
+    forward_deserialize!(deserialize_i16);
+    forward_deserialize!(deserialize_i32);
+    forward_deserialize!(deserialize_i64);
+    forward_deserialize!(deserialize_f32);
+    forward_deserialize!(deserialize_f64);
+    forward_deserialize!(deserialize_char);
+    forward_deserialize!(deserialize_str);
+    forward_deserialize!(deserialize_string);
+    forward_deserialize!(deserialize_unit);
+    forward_deserialize!(deserialize_option);
+    forward_deserialize!(deserialize_seq);
+    forward_deserialize!(deserialize_seq_fixed_size, len => usize);
+    forward_deserialize!(deserialize_bytes);
+    forward_deserialize!(deserialize_byte_buf);
+    forward_deserialize!(deserialize_map);
+    forward_deserialize!(deserialize_unit_struct, name => &'static str);
+    forward_deserialize!(deserialize_newtype_struct, name => &'static str);
+    forward_deserialize!(deserialize_tuple_struct, name => &'static str, len => usize);
+    forward_deserialize!(deserialize_struct,
+                         name => &'static str,
+                         fields => &'static [&'static str]);
+    forward_deserialize!(deserialize_struct_field);
+    forward_deserialize!(deserialize_tuple, len => usize);
+    forward_deserialize!(deserialize_enum,
+                         name => &'static str,
+                         variants => &'static [&'static str]);
+    forward_deserialize!(deserialize_ignored_any);
+}
+
+struct Visitor<'a, V> {
+    visitor: V,
+    key: Option<&'a Key>,
+}
+
+impl<'a, V> Visitor<'a, V> {
+    fn expand_str<E>(&self, s: &str) -> Result<Option<String>, E>
+        where E: de::Error
+    {
+        if s.starts_with("${enc:") && s.ends_with("}") {
+            match self.key {
+                Some(key) => {
+                    match key.decrypt(&s[6..s.len() - 1]) {
+                        Ok(s) => Ok(Some(s)),
+                        Err(e) => Err(E::custom(&e.to_string())),
+                    }
+                }
+                None => Err(E::custom("missing encryption key")),
+            }
+        } else {
+            Ok(None)
+        }
+    }
+}
+
+macro_rules! forward_visit {
+    ($name:ident, $ty:ty) => {
+        fn $name<E>(self, v: $ty) -> Result<V::Value, E>
+            where E: de::Error
+        {
+            self.visitor.$name(v)
+        }
+    }
+}
+
+impl<'a, V> de::Visitor for Visitor<'a, V>
+    where V: de::Visitor
+{
+    type Value = V::Value;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        self.visitor.expecting(formatter)
+    }
+
+    forward_visit!(visit_bool, bool);
+    forward_visit!(visit_i8, i8);
+    forward_visit!(visit_i16, i16);
+    forward_visit!(visit_i32, i32);
+    forward_visit!(visit_i64, i64);
+    forward_visit!(visit_u8, u8);
+    forward_visit!(visit_u16, u16);
+    forward_visit!(visit_u32, u32);
+    forward_visit!(visit_u64, u64);
+    forward_visit!(visit_f32, f32);
+    forward_visit!(visit_f64, f64);
+    forward_visit!(visit_char, char);
+    forward_visit!(visit_bytes, &[u8]);
+    forward_visit!(visit_byte_buf, Vec<u8>);
+
+    fn visit_str<E>(self, v: &str) -> Result<V::Value, E>
+        where E: de::Error
+    {
+        match self.expand_str(v)? {
+            Some(s) => self.visitor.visit_string(s),
+            None => self.visitor.visit_str(v),
+        }
+    }
+
+    fn visit_string<E>(self, v: String) -> Result<V::Value, E>
+        where E: de::Error
+    {
+        match self.expand_str(&v)? {
+            Some(s) => self.visitor.visit_string(s),
+            None => self.visitor.visit_string(v),
+        }
+    }
+
+    fn visit_unit<E>(self) -> Result<V::Value, E>
+        where E: de::Error
+    {
+        self.visitor.visit_unit()
+    }
+
+    fn visit_none<E>(self) -> Result<V::Value, E>
+        where E: de::Error
+    {
+        self.visitor.visit_none()
+    }
+
+    fn visit_some<D>(self, deserializer: D) -> Result<V::Value, D::Error>
+        where D: de::Deserializer
+    {
+        let deserializer = Deserializer::new(deserializer, self.key);
+        self.visitor.visit_some(deserializer)
+    }
+
+    fn visit_newtype_struct<D>(self, deserializer: D) -> Result<V::Value, D::Error>
+        where D: de::Deserializer
+    {
+        let deserializer = Deserializer::new(deserializer, self.key);
+        self.visitor.visit_newtype_struct(deserializer)
+    }
+
+    fn visit_seq<V2>(self, visitor: V2) -> Result<V::Value, V2::Error>
+        where V2: de::SeqVisitor
+    {
+        let visitor = Visitor {
+            visitor: visitor,
+            key: self.key,
+        };
+        self.visitor.visit_seq(visitor)
+    }
+
+    fn visit_map<V2>(self, visitor: V2) -> Result<V::Value, V2::Error>
+        where V2: de::MapVisitor
+    {
+        let visitor = Visitor {
+            visitor: visitor,
+            key: self.key,
+        };
+        self.visitor.visit_map(visitor)
+    }
+
+    fn visit_enum<V2>(self, visitor: V2) -> Result<V::Value, V2::Error>
+        where V2: de::EnumVisitor
+    {
+        let visitor = Visitor {
+            visitor: visitor,
+            key: self.key,
+        };
+        self.visitor.visit_enum(visitor)
+    }
+}
+
+impl<'a, V> de::SeqVisitor for Visitor<'a, V>
+    where V: de::SeqVisitor
+{
+    type Error = V::Error;
+
+    fn visit_seed<T>(&mut self, seed: T) -> Result<Option<T::Value>, V::Error>
+        where T: de::DeserializeSeed
+    {
+        let seed = DeserializeSeed {
+            seed: seed,
+            key: self.key,
+        };
+        self.visitor.visit_seed(seed)
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.visitor.size_hint()
+    }
+}
+
+impl<'a, V> de::MapVisitor for Visitor<'a, V>
+    where V: de::MapVisitor
+{
+    type Error = V::Error;
+
+    fn visit_key_seed<K>(&mut self, seed: K) -> Result<Option<K::Value>, V::Error>
+        where K: de::DeserializeSeed
+    {
+        let seed = DeserializeSeed {
+            seed: seed,
+            key: self.key,
+        };
+        self.visitor.visit_key_seed(seed)
+    }
+
+    fn visit_value_seed<T>(&mut self, seed: T) -> Result<T::Value, V::Error>
+        where T: de::DeserializeSeed
+    {
+        let seed = DeserializeSeed {
+            seed: seed,
+            key: self.key,
+        };
+        self.visitor.visit_value_seed(seed)
+    }
+
+    fn visit_seed<K, T>(&mut self,
+                        kseed: K,
+                        vseed: T)
+                        -> Result<Option<(K::Value, T::Value)>, V::Error>
+        where K: de::DeserializeSeed,
+              T: de::DeserializeSeed
+    {
+        let kseed = DeserializeSeed {
+            seed: kseed,
+            key: self.key,
+        };
+        let vseed = DeserializeSeed {
+            seed: vseed,
+            key: self.key,
+        };
+        self.visitor.visit_seed(kseed, vseed)
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.visitor.size_hint()
+    }
+}
+
+impl<'a, V> de::EnumVisitor for Visitor<'a, V>
+    where V: de::EnumVisitor
+{
+    type Error = V::Error;
+    type Variant = Visitor<'a, V::Variant>;
+
+    fn visit_variant_seed<T>(self, seed: T) -> Result<(T::Value, Visitor<'a, V::Variant>), V::Error>
+        where T: de::DeserializeSeed
+    {
+        let seed = DeserializeSeed {
+            seed: seed,
+            key: self.key,
+        };
+        match self.visitor.visit_variant_seed(seed) {
+            Ok((value, variant)) => {
+                let variant = Visitor {
+                    visitor: variant,
+                    key: self.key,
+                };
+                Ok((value, variant))
+            }
+            Err(e) => Err(e),
+        }
+    }
+}
+
+impl<'a, V> de::VariantVisitor for Visitor<'a, V>
+    where V: de::VariantVisitor
+{
+    type Error = V::Error;
+
+    fn visit_unit(self) -> Result<(), V::Error> {
+        self.visitor.visit_unit()
+    }
+
+    fn visit_newtype_seed<T>(self, seed: T) -> Result<T::Value, V::Error>
+        where T: de::DeserializeSeed
+    {
+        let seed = DeserializeSeed {
+            seed: seed,
+            key: self.key,
+        };
+        self.visitor.visit_newtype_seed(seed)
+    }
+
+    fn visit_tuple<V2>(self, len: usize, visitor: V2) -> Result<V2::Value, V::Error>
+        where V2: de::Visitor
+    {
+        let visitor = Visitor {
+            visitor: visitor,
+            key: self.key,
+        };
+        self.visitor.visit_tuple(len, visitor)
+    }
+
+    fn visit_struct<V2>(self,
+                        fields: &'static [&'static str],
+                        visitor: V2)
+                        -> Result<V2::Value, V::Error>
+        where V2: de::Visitor
+    {
+        let visitor = Visitor {
+            visitor: visitor,
+            key: self.key,
+        };
+        self.visitor.visit_struct(fields, visitor)
+    }
+}
+
+struct DeserializeSeed<'a, T> {
+    seed: T,
+    key: Option<&'a Key>,
+}
+
+impl<'a, T> de::DeserializeSeed for DeserializeSeed<'a, T>
+    where T: de::DeserializeSeed
+{
+    type Value = T::Value;
+
+    fn deserialize<D>(self, deserializer: D) -> Result<T::Value, D::Error>
+        where D: de::Deserializer
+    {
+        let deserializer = Deserializer::new(deserializer, self.key);
+        self.seed.deserialize(deserializer)
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,286 @@
+//! Tooling for deserialization of partially encrypted configuration files.
+//!
+//! Application configurations typically consist mostly of non-sensitive information, with a few
+//! bits of information that is sensitive such as authentication secrets or cookie encryption keys.
+//! Storing those sensitive values in an encrypted form at rest can defend against leakage when,
+//! for example, copy/pasting the config as long as the encryption key is not additionally leaked.
+//!
+//! # Usage
+//!
+//! Assume we have a `conf/encrypted-config-value.key` file that looks like:
+//!
+//! ```not_rust
+//! AES:NwQZdNWsFmYMCNSQlfYPDJtFBgPzY8uZlFhMCLnxNQE=
+//! ```
+//!
+//! And a `conf/config.json` file that looks like:
+//!
+//! ```json
+//! {
+//!     "secret_value": "${enc:5BBfGvf90H6bApwfxUjNdoKRW1W+GZCbhBuBpzEogVBmQZyWFFxcKyf+UPV5FOhrw/wrVZyoL3npoDfYjPQV/zg0W/P9cVOw}",
+//!     "non_secret_value": "hello, world!"
+//! }
+//! ```
+//!
+//! ```no_run
+//! extern crate serde;
+//! extern crate serde_json;
+//! extern crate serde_encrypted_value;
+//!
+//! #[macro_use]
+//! extern crate serde_derive;
+//!
+//! use serde::Deserialize;
+//! use std::io::Read;
+//! use std::fs::File;
+//!
+//! #[derive(Deserialize)]
+//! struct Config {
+//!     secret_value: String,
+//!     non_secret_value: String,
+//! }
+//!
+//! fn main() {
+//!     let key = "conf/encrypted-config-value.key";
+//!     let key = serde_encrypted_value::Key::from_file(key)
+//!         .unwrap();
+//!
+//!     let mut config = vec![];
+//!     File::open("conf/config.json")
+//!         .unwrap()
+//!         .read_to_end(&mut config)
+//!         .unwrap();
+//!
+//!     let mut deserializer = serde_json::Deserializer::from_slice(&config);
+//!     let deserializer = serde_encrypted_value::Deserializer::new(
+//!         &mut deserializer, key.as_ref());
+//!     let config = Config::deserialize(deserializer).unwrap();
+//!
+//!     assert_eq!(config.secret_value, "L/TqOWz7E4z0SoeiTYBrqbqu");
+//!     assert_eq!(config.non_secret_value, "hello, world!");
+//! }
+//! ```
+#![warn(missing_docs)]
+
+extern crate base64;
+extern crate openssl;
+extern crate serde;
+
+#[macro_use]
+extern crate error_chain;
+
+#[cfg(test)]
+extern crate serde_json;
+#[cfg(test)]
+extern crate tempdir;
+
+#[cfg(test)]
+#[macro_use]
+extern crate serde_derive;
+
+use openssl::symm::{self, Cipher};
+use openssl::rand::rand_bytes;
+use std::fmt;
+use std::fs::File;
+use std::io::{self, Read};
+use std::path::Path;
+use std::str::FromStr;
+
+use errors::*;
+
+pub use deserializer::Deserializer;
+
+const KEY_PREFIX: &'static str = "AES:";
+const KEY_LEN: usize = 32;
+const IV_LEN: usize = 32;
+const TAG_LEN: usize = 16;
+
+mod deserializer;
+
+/// Errors
+pub mod errors {
+    error_chain!{}
+}
+
+/// A key used to encrypt or decrypt values. It represents both an algorithm and a key.
+///
+/// The canonical serialized representation of a `Key` is a string consisting of an algorithm
+/// identifier, followed by a `:`, followed by the base64 encoded bytes of the key. The `Display`
+/// and `FromStr` implementations serialize and deserialize in this format.
+///
+/// The only algorithm currently supported is AES 256 GCM, which uses the identifier `AES`.
+pub struct Key(Vec<u8>);
+
+impl Key {
+    /// Creates a random AES key.
+    pub fn random_aes() -> Result<Key> {
+        let mut key = vec![0; KEY_LEN];
+        rand_bytes(&mut key).chain_err(|| "error generating random key")?;
+        Ok(Key(key))
+    }
+
+    /// A convenience function which deserializes a `Key` from a file.
+    ///
+    /// If the file does not exist, `None` is returned. Otherwise, the contents of the file are
+    /// parsed via `Key`'s `FromStr` implementation.
+    pub fn from_file<P>(path: P) -> Result<Option<Key>>
+        where P: AsRef<Path>
+    {
+        let mut file = match File::open(path) {
+            Ok(file) => file,
+            Err(ref e) if e.kind() == io::ErrorKind::NotFound => return Ok(None),
+            Err(e) => {
+                // FIXME ew :(
+                Err(e).chain_err(|| "error opening key")?;
+                unreachable!();
+            }
+        };
+        let mut s = String::new();
+        file.read_to_string(&mut s).chain_err(|| "error reading key")?;
+        s.parse().map(Some)
+    }
+
+    /// Encrypts a string with this key.
+    pub fn encrypt(&self, value: &str) -> Result<String> {
+        let mut iv = [0; IV_LEN];
+        rand_bytes(&mut iv).chain_err(|| "error generating nonce")?;
+
+        let mut tag = [0; TAG_LEN];
+
+        let cipher = Cipher::aes_256_gcm();
+        let ct = symm::encrypt_aead(cipher, &self.0, Some(&iv), &[], value.as_bytes(), &mut tag)
+            .chain_err(|| "error encrypting value")?;
+
+        let value = iv.iter().chain(ct.iter()).chain(tag.iter()).cloned().collect::<Vec<_>>();
+
+        Ok(base64::encode(&value))
+    }
+
+    /// Decrypts a string with this key.
+    pub fn decrypt(&self, value: &str) -> Result<String> {
+        let value = base64::decode(&value).chain_err(|| "error decoding encrypted value")?;
+
+        if value.len() < IV_LEN + TAG_LEN {
+            bail!("encrypted value too short");
+        }
+
+        let (iv, value) = value.split_at(IV_LEN);
+        let (ct, tag) = value.split_at(value.len() - TAG_LEN);
+
+        let cipher = Cipher::aes_256_gcm();
+        let pt = symm::decrypt_aead(cipher, &self.0, Some(iv), &[], ct, tag)
+            .chain_err(|| "error decrypting value")?;
+        let pt = String::from_utf8(pt).chain_err(|| "error decrypting value")?;
+
+        Ok(pt)
+    }
+}
+
+impl fmt::Display for Key {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        write!(fmt, "AES:{}", base64::encode(&self.0))
+    }
+}
+
+impl FromStr for Key {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Key> {
+        if !s.starts_with(KEY_PREFIX) {
+            bail!("invalid key prefix");
+        }
+
+        let key = base64::decode(&s[KEY_PREFIX.len()..]).chain_err(|| "error decoding key")?;
+        Ok(Key(key))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use serde::Deserialize;
+    use tempdir::TempDir;
+    use std::fs::File;
+    use std::io::Write;
+
+    use super::*;
+
+    const KEY: &'static str = "AES:NwQZdNWsFmYMCNSQlfYPDJtFBgPzY8uZlFhMCLnxNQE=";
+
+    #[test]
+    fn from_file_aes() {
+        let dir = TempDir::new("from_file_aes").unwrap();
+        let path = dir.path().join("encrypted-config-value.key");
+        let mut key = File::create(&path).unwrap();
+        key.write_all(KEY.as_bytes()).unwrap();
+
+        assert!(Key::from_file(&path).unwrap().is_some());
+    }
+
+    #[test]
+    fn from_file_empty() {
+        let dir = TempDir::new("from_file_aes").unwrap();
+        let path = dir.path().join("encrypted-config-value.key");
+
+        assert!(Key::from_file(&path).unwrap().is_none());
+    }
+
+    #[test]
+    fn decrypt() {
+        let ct = "5BBfGvf90H6bApwfxUjNdoKRW1W+GZCbhBuBpzEogVBmQZyWFFxcKyf+UPV5FOhrw/wrVZyoL3npoDfYj\
+                  PQV/zg0W/P9cVOw";
+        let pt = "L/TqOWz7E4z0SoeiTYBrqbqu";
+
+        let key: Key = KEY.parse().unwrap();
+        let actual = key.decrypt(ct).unwrap();
+        assert_eq!(actual, pt);
+    }
+
+    #[test]
+    fn encrypt_decrypt() {
+        let key = Key::random_aes().unwrap();
+        let pt = "L/TqOWz7E4z0SoeiTYBrqbqu";
+        let ct = key.encrypt(pt).unwrap();
+        let actual = key.decrypt(&ct).unwrap();
+        assert_eq!(pt, actual);
+    }
+
+    #[test]
+    fn deserializer() {
+        #[derive(Deserialize, PartialEq, Debug)]
+        struct Config {
+            sub: Subconfig,
+        }
+
+        #[derive(Deserialize, PartialEq, Debug)]
+        struct Subconfig {
+            encrypted: Vec<String>,
+            plaintext: String,
+        }
+
+        let config = r#"
+{
+    "sub": {
+        "encrypted": [
+            "${enc:5BBfGvf90H6bApwfxUjNdoKRW1W+GZCbhBuBpzEogVBmQZyWFFxcKyf+UPV5FOhrw/wrVZyoL3npoDfYjPQV/zg0W/P9cVOw}"
+        ],
+        "plaintext": "${foobar}"
+    }
+}
+        "#;
+
+        let key = KEY.parse().unwrap();
+        let mut deserializer = serde_json::Deserializer::from_str(config);
+        let deserializer = Deserializer::new(&mut deserializer, Some(&key));
+
+        let config = Config::deserialize(deserializer).unwrap();
+
+        let expected = Config {
+            sub: Subconfig {
+                encrypted: vec!["L/TqOWz7E4z0SoeiTYBrqbqu".to_string()],
+                plaintext: "${foobar}".to_string(),
+            },
+        };
+
+        assert_eq!(config, expected);
+    }
+}
