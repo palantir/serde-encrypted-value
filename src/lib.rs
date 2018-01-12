ccd0da532eb8ceef49c068d9e515a7f0e685504e
Steven Fackler <sfackler@palantir.com>
2018-01-11 17:09:42 -0800

    Doc link fixes

--- src/lib.rs
+++ src/lib.rs
@@ -75,7 +75,7 @@
 //! }
 //! ```
 #![warn(missing_docs)]
-#![doc(html_root_url="https://docs.rs/serde-encrypted-value/0.2.0")]
+#![doc(html_root_url="https://docs.rs/serde-encrypted-value/0.3")]
 
 extern crate base64;
 extern crate openssl;
